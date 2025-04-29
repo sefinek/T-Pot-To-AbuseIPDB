6d1631435934b998ba75016f76439fd9b66b8eec
Sefinek <contact@sefinek.net>
2025-04-29 08:50:27 +0200

    Cleanup

--- deprecated/utils/repo.js
+++
@@ -1,8 +0,0 @@
-const { version, homepage } = require('../../package.json');
-
-const match = homepage.match(/github\.com\/([^\\/]+)\/([^#\\/]+)/);
-const repoFull = match ? `${match[1]}/${match[2]}` : 'Missing/data';
-const repoName = match ? match[2] : 'Unknown';
-const repoURL = homepage.split('#')[0] || '';
-
-module.exports = { version, repoFull, repoName, repoURL };
